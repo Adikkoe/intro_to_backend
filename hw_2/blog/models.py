ae432ad611da6d0f603b72c8d5b2df9e92e38a78
Adilet <adiletabubakyr8@gmail.com>
2025-02-18 10:21:22 +0500

    hw_2 and 3

---
+++ hw_2/blog/models.py
@@ -0,0 +1,8 @@
+# blog/models.py
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
